06be734c02f26fc8e72cee9ceb2b51c6e35a081d
Brian Peterson <bepetersn@gmail.com>
2014-05-15 16:17:39 -0500

    separate normalize into its own function

--- db/migrate_charges.py
+++ db/migrate_charges.py
@@ -3,10 +3,43 @@ from pprint import pprint
 import re
 
 
-SQLITE_DB = 'charges.db'
+def read_charges(sqlite_db='charges.db'):
 
-CHARGE_REGEX = \
-        ('(\d{1,4})'                 # - Chapter
+    """ 
+        Assumes the SQL table 'chargeshistory' exists, with at least a 
+        citation column in it, basically in the format of the data found at:
+        "http://cookcountyjail.recoveredfactory.net/api/1.0/chargeshistory/?format=json".
+
+    """
+
+    conn = sqlite3.connect(sqlite_db)
+    c = conn.cursor()
+
+    # get the size of the table
+    c.execute('SELECT COUNT(*) FROM chargeshistory')
+    for result in c:
+        size = float(result[0])
+
+    c.execute("SELECT citation FROM chargeshistory;")
+
+    charges = []
+
+    for row in c:
+        charges.append(row[0])
+
+    return charges
+
+
+def normalize(all_charges=None):
+
+    """
+        Returns a tuple containing:
+
+        - a list of unique, normalized citations, 
+        - a list of unqiue citations that couldn't be handled    """
+
+    CHARGE_REGEX = \
+        ('(\d{1,3})'                 # - Chapter
         ' ILCS '                     #  
         '(\d{1,4})'                  # - Act
         '[ |/]'                      # 
@@ -16,46 +49,17 @@ CHARGE_REGEX = \
         '((?:\([\w|\-|.]\)){0,4})?'  # - optional sub-sections
         '.*')                        # 
 
-def normalize():
-
-    """ 
-        Assumes the SQL table 'chargeshistory' exists, with at least a 
-        citation column in it, basically in the format of the data found at:
-        "http://cookcountyjail.recoveredfactory.net/api/1.0/chargeshistory/?format=json".
-
-        SELECTS from the 'chargeshistory' table before attempting to 
-        normalize the resultant data. Returns a 4-tuple:
-
-        - a list of normalized charge objects (tuples of a numeric id and citation), 
-        - a list of unqiue citations that couldn't be handled, 
-        - the percent of the entire input table that couldn't be handled 
-        - the percent of the unique citations that couldn't be handled
-
-    """
-
     CHARGE_PATTERN = re.compile(CHARGE_REGEX)
 
-    charges = []
+    if not all_charges:
+        all_charges = read_charges()
 
-    conn = sqlite3.connect(SQLITE_DB)
-    conn.enable_load_extension(True)
-    conn.load_extension("/usr/lib/sqlite3/pcre.so")
-    c = conn.cursor()
-
-    # get the size of the table
-    c.execute('SELECT COUNT(*) FROM chargeshistory')
-    for result in c:
-        size = float(result[0])
-
-    c.execute("SELECT citation FROM chargeshistory WHERE "
-              "citation REGEXP '{0}'".format(CHARGE_REGEX))
-
-    charge_id = 0
+    uniq_charges = set([])
     total_fail = 0
     uniq_fail = set([])
-    for row in c:
 
-        charge = row[0]
+    for charge in all_charges:
+
         match = CHARGE_PATTERN.match(charge)
 
         if match:
@@ -65,36 +69,30 @@ def normalize():
             section = match.group(3)
             subsection = match.group(4)
 
-            # ignore id creation until we know it's a new charge
             citation = "{0} {1}/{2}{3}".format(chapter, act, section, subsection)
-            if citation not in [c[1] for c in charges]:
-                charges.append((charge_id, citation))
-
-            # increment if a new charge was added
-            if len(charges) > charge_id:
-                charge_id += 1
+            uniq_charges.add(citation)
 
         else:
             total_fail += 1
             uniq_fail.add(charge) 
 
-    total_fail_rate = round((total_fail / size), 3)
-    uniq_fail_rate = round((len(uniq_fail) / float(len(charges))), 3)
-
-    return charges, uniq_fail, total_fail_rate, uniq_fail_rate
+    return uniq_charges, uniq_fail
 
 
-def write(charges=None):
+def write_migration(charges=None, sqlite_db='charges.db'):
 
-    conn = sqlite3.connect(SQLITE_DB)
+    """ Creates a sql file for inserting the data of the 'charges'
+        variable into a table. Assumes there is a 'statues' table in
+        the sqlite_db database. """
 
     if not charges:
-        charges = normalize()
+        charges, _ = normalize()
 
-    query_str = 'INSERT INTO charges VALUES (?, ?)'
-    conn.executemany(query_str, charges)
-    conn.commit()
+    with open('charge_migration.sql', 'w') as migration_file:
 
-    print "{0} citations added to the charges table".format(len(charges))
+        query_str = "INSERT INTO statutes VALUES ('{0}');\n"
+        for c in charges:
+            migration_file.write(query_str.format(c))
 
+    print "{0} citations added to the migration".format(len(charges))
 
